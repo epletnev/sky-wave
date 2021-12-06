04b43141623119bac61c4d33469d942004f0b812
epletnev <e.a.pletnev@gmail.com>
2021-12-06 10:32:26 +0000

    updated project for deployment

--- src/redux/contact/contactAction.js
+++ src/redux/contact/contactAction.js
@@ -1,29 +1,71 @@
 import { GET_CONTACT, GET_ENQUIRY, ADD_ENQUIRY } from "./contactType";
-import axios from "axios";
+// import axios from "axios";
+
+const contactData = {
+  address: "2903 Avenue Z, MG Marg, Mumbai, India",
+  phone: ["(91) 987 654 3210", "(91) 987 654 3211"],
+  email: "contact@skywave.com",
+};
+
+const getInTouchData = [
+  {
+    name: "aravind",
+    email: "aravindjai0@gmail.com",
+    phone: "3333333123",
+    message: "Hi arathe",
+    id: 1,
+  },
+];
 
 // get contact data
-export const getContact = () => async (dispatch) => {
-  const result = await axios.get("http://localhost:5000/contactData");
+export const getContact = () => (dispatch) => {
+  const result = { ...contactData };
   dispatch({
     type: GET_CONTACT,
-    payload: result.data,
+    payload: result,
   });
 };
 
 // get contact enquiry data
-export const getEnquiry = () => async (dispatch) => {
-  const result = await axios.get("http://localhost:5000/getInTouchData");
+export const getEnquiry = () => (dispatch) => {
+  const result = [...getInTouchData];
   dispatch({
     type: GET_ENQUIRY,
-    payload: result.data,
+    payload: result,
   });
 };
 
 // add contact enquiry data
-export const addEnquiry = (newEnquiry) => async (dispatch) => {
-  await axios.post("http://localhost:5000/getInTouchData", newEnquiry);
+export const addEnquiry = (newEnquiry) => (dispatch) => {
   dispatch({
     type: ADD_ENQUIRY,
     payload: newEnquiry,
   });
 };
+
+// // get contact data
+// export const getContact = () => async (dispatch) => {
+//   const result = await axios.get("http://localhost:5000/contactData");
+//   dispatch({
+//     type: GET_CONTACT,
+//     payload: result.data,
+//   });
+// };
+
+// // get contact enquiry data
+// export const getEnquiry = () => async (dispatch) => {
+//   const result = await axios.get("http://localhost:5000/getInTouchData");
+//   dispatch({
+//     type: GET_ENQUIRY,
+//     payload: result.data,
+//   });
+// };
+
+// // add contact enquiry data
+// export const addEnquiry = (newEnquiry) => async (dispatch) => {
+//   await axios.post("http://localhost:5000/getInTouchData", newEnquiry);
+//   dispatch({
+//     type: ADD_ENQUIRY,
+//     payload: newEnquiry,
+//   });
+// };
--- src/redux/menuDetails/menuDetailsAction.js
+++ src/redux/menuDetails/menuDetailsAction.js
@@ -1,28 +1,223 @@
 import { GET_MENU_DETAILS, GET_REVIEWS, ADD_REVIEW } from "./menuDetailsType";
-import axios from "axios";
+// import axios from "axios";
+
+const data = [
+  {
+    id: 1,
+    name: "Snack Set",
+    description: "Butter, cream, honey, salty cottage cheese, white cheese.",
+    price: 8.2,
+    image: {
+      small: "assets/images/foods/breakfast/1/small.jpg",
+      medium: "assets/images/foods/breakfast/1/medium.jpg",
+      big: "assets/images/foods/breakfast/1/big.jpg",
+    },
+    discount: 0,
+    ratingsCount: 4,
+    ratingsValue: 350,
+    availibilityCount: 5,
+    cartCount: 0,
+    weight: 160,
+    isVegetarian: false,
+    categoryId: "Breakfast",
+    reviews: [],
+  },
+  {
+    id: 2,
+    name: "Salmon And Avocado Bruschetta",
+    description:
+      "Black bread, smoked salmon, Philadelphia cheese, avocado, tomatoes, eggs.",
+    price: 12.5,
+    image: {
+      small: "assets/images/foods/breakfast/2/small.jpg",
+      medium: "assets/images/foods/breakfast/2/medium.jpg",
+      big: "assets/images/foods/breakfast/2/big.jpg",
+    },
+    discount: 10,
+    ratingsCount: 4,
+    ratingsValue: 320,
+    availibilityCount: 7,
+    cartCount: 0,
+    weight: 180,
+    isVegetarian: false,
+    categoryId: "Breakfast",
+    reviews: [
+      {
+        name: "aravind",
+        email: "aravindjai0@gmail.com",
+        phone: "3333333123",
+        FoodQuality: "Average",
+        MenuVariety: "Not Satisfied",
+        Service: "Average",
+        Atmosphere: "Average",
+        message:
+          "complete this eros and my ecological financing. The said ugly needs a great cartoon flavor. As expected sometimes vengeful. Therefore, a sad scenario for the kids, to put down the housing, the biggest big thing. Aeneas feasibility, the torturer of the pregnant element to stop, just the vehicle of the arches, the need of the bow of the earth in a great way.",
+        rating: 5,
+        date: "2021-09-19T20:56:13.084Z",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Croque Madame",
+    description:
+      "Toasted bread, veal salami, eggs, butter, Gouda cheese, arugula, cucumber, radish.",
+    price: 9.5,
+    image: {
+      small: "assets/images/foods/breakfast/3/small.jpg",
+      medium: "assets/images/foods/breakfast/3/medium.jpg",
+      big: "assets/images/foods/breakfast/3/big.jpg",
+    },
+    discount: 0,
+    ratingsCount: 4,
+    ratingsValue: 370,
+    availibilityCount: 5,
+    cartCount: 0,
+    weight: 125,
+    isVegetarian: false,
+    categoryId: "Dinner",
+    reviews: [
+      {
+        name: "Ram",
+        email: "email@gmail.com",
+        phone: "7283988923",
+        FoodQuality: "Average",
+        MenuVariety: "Average",
+        Service: "Not Satisfied",
+        Atmosphere: "Average",
+        message:
+          "complete this eros and my ecological financing. The said ugly needs a great cartoon flavor. As expected sometimes vengeful. Therefore, a sad scenario for the kids, to put down the housing, the biggest big thing. Aeneas feasibility, the torturer of the pregnant element to stop, just the vehicle of the arches, the need of the bow of the earth in a great way.",
+        rating: 3,
+        date: "2021-09-20T03:51:53.474Z",
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Cheese Pancakes",
+    description:
+      "Puff pastry, cheese filling, arugula, carrot, cucumber, radish.",
+    price: 7.3,
+    image: {
+      small: "assets/images/foods/breakfast/4/small.jpg",
+      medium: "assets/images/foods/breakfast/4/medium.jpg",
+      big: "assets/images/foods/breakfast/4/big.jpg",
+    },
+    discount: 0,
+    ratingsCount: 4,
+    ratingsValue: 380,
+    availibilityCount: 7,
+    cartCount: 0,
+    weight: 110,
+    isVegetarian: false,
+    categoryId: "Lunch",
+    reviews: [],
+  },
+  {
+    id: 5,
+    name: "Cheese Sandwich",
+    description:
+      "Toasted bread, Cheddar cheese, Gouda cheese, butter, arugula, cucumber, radish.",
+    price: 7.3,
+    image: {
+      small: "assets/images/foods/breakfast/5/small.jpg",
+      medium: "assets/images/foods/breakfast/5/medium.jpg",
+      big: "assets/images/foods/breakfast/5/big.jpg",
+    },
+    discount: 0,
+    ratingsCount: 5,
+    ratingsValue: 480,
+    availibilityCount: 7,
+    cartCount: 0,
+    weight: 110,
+    isVegetarian: false,
+    categoryId: "Breakfast",
+    reviews: [],
+  },
+  {
+    id: 31,
+    name: "Red Wine",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    price: 25.5,
+    image: {
+      small: "assets/images/foods/drinks/1/small.jpg",
+      medium: "assets/images/foods/drinks/1/medium.jpg",
+      big: "assets/images/foods/drinks/1/big.jpg",
+    },
+    discount: 0,
+    ratingsCount: 4,
+    ratingsValue: 480,
+    availibilityCount: 5,
+    cartCount: 0,
+    weight: 250,
+    isVegetarian: false,
+    categoryId: "Lunch",
+    reviews: [
+      {
+        name: "Akash",
+        email: "as@gmail.com",
+        phone: "9998897888",
+        FoodQuality: "Average",
+        MenuVariety: "Average",
+        Service: "Not Satisfied",
+        Atmosphere: "Satisfied",
+        message:
+          "A review of a situation or system is its formal examination by people in authority. ... When a military or political leader reviews troops, they examine them, or watch them marching.\n",
+        rating: 3,
+        date: "2021-09-20T04:21:35.155Z",
+      },
+    ],
+  },
+];
 
 // get menu details page data
-export const getMenuDetails = (id) => async (dispatch) => {
-  const result = await axios.get(`http://localhost:5000/data/${id}`);
+export const getMenuDetails = (id) => (dispatch) => {
+  const result = data.filter((menuItem) => menuItem.id === id);
   dispatch({
     type: GET_MENU_DETAILS,
-    payload: result.data,
+    payload: result[0],
   });
 };
 
 // get reviews data
-export const getReviews = (id) => async (dispatch) => {
-  const result = await axios.get(`http://localhost:5000/data/${id}`);
+export const getReviews = (id) => (dispatch) => {
+  const result = data.filter((menuItem) => menuItem.id === id);
   dispatch({
     type: GET_REVIEWS,
-    payload: result.data,
+    payload: result[0],
   });
 };
 
 // add review data
-export const addReview = (newReview) => async (dispatch) => {
+export const addReview = (newReview) => (dispatch) => {
   dispatch({
     type: ADD_REVIEW,
     payload: newReview,
   });
 };
+
+// // get menu details page data
+// export const getMenuDetails = (id) => async (dispatch) => {
+//   const result = await axios.get(`http://localhost:5000/data/${id}`);
+//   dispatch({
+//     type: GET_MENU_DETAILS,
+//     payload: result.data,
+//   });
+// };
+
+// // get reviews data
+// export const getReviews = (id) => async (dispatch) => {
+//   const result = await axios.get(`http://localhost:5000/data/${id}`);
+//   dispatch({
+//     type: GET_REVIEWS,
+//     payload: result.data,
+//   });
+// };
+
+// // add review data
+// export const addReview = (newReview) => async (dispatch) => {
+//   dispatch({
+//     type: ADD_REVIEW,
+//     payload: newReview,
+//   });
+// };
